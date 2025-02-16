3dea57070be49daa15d9cfed814264e3eb3d7fb3
sharjeelremote <sharjeelremote@gmail.com>
2025-02-17 04:26:00 +0500

    lufthansa page added and css updated

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("header").innerHTML =
+  '<object type="text/html" data="components/header.html"></object>';
+document.getElementById("footer").innerHTML =
+  '<object type="text/html" data="components/footer.html"></object>';
